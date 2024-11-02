78c34bf938c49e691cfe2d2a681ff78f674c96b7
Timmy Jose <zoltan.jose@gmail.com>
2024-11-02 17:42:43 +0530

    chore: basic i/o shell

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+mod ast;
+mod evaluator;
+mod lexer;
+mod parser;
---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use std::io::{self, Write};
+
+const SPLASH: &str = r#"
+  /_\__      _____  ___  ___  _ __ ___   ___  / __\__ _| | ___
+ //_\\ \ /\ / / _ \/ __|/ _ \| '_ ` _ \ / _ \/ /  / _` | |/ __|
+/  _  \ V  V /  __/\__ \ (_) | | | | | |  __/ /__| (_| | | (__
+\_/ \_/\_/\_/ \___||___/\___/|_| |_| |_|\___\____/\__,_|_|\___|
+
+Welcome! v1.0.0
+"#;
+
+const PROMPT: &str = ">> ";
+
+fn get_input() -> eyre::Result<String> {
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input)?;
+    Ok(input.trim().to_owned())
+}
+
+fn main() -> eyre::Result<()> {
+    println!("{SPLASH}");
+
+    loop {
+        print!("{PROMPT}");
+        io::stdout().lock().flush()?;
+        let input = get_input()?;
+        println!("{input}");
+    }
+}
